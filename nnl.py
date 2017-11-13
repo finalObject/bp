7582825556f6c7c953f1eac1f0c0d4e9fa6ec401
X. HU(Mac) <i@finalobject.cn>
2017-11-13 10:33:24 +0800

    good morning

---
+++ nnl.py
@@ -0,0 +1,8 @@
+import random
+import math
+class Neuron:
+	def __init__(self)
+class NeuronLayer:
+	def __init__(self)
+class NeuralNetwork:
+	def __init__(self)
---
+++ test.py
@@ -0,0 +1,3 @@
+import matplotlib.pyplot as plt
+x=[]
+plt.plot(x, b, label='x', color="green", linewidth=1)
